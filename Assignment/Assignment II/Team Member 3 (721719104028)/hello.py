064221ea9c02ce75ae59dc9957512a6454098e98
guhan26 <92621227+guhan26@users.noreply.github.com>
2022-11-05 19:23:22 +0530

    Update hello.py

--- Assignment/Assignment II/Team Member 3 (721719104028)/hello.py
+++ Assignment/Assignment II/Team Member 3 (721719104028)/hello.py
@@ -2,25 +2,25 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 
 def home():
-   #return "hello hemasai"
+   #return "hello guhan"
    return render_template('index.html')
 
 @app.route("/about")
 def about():
-   #return "hello hemasai"
+   #return "hello guhan"
    return render_template('about.html')
 
 @app.route("/signup")
 def signup():
-   #return "hello hemasai"
+   #return "hello guhan"
    return render_template('signup.html')
 
 @app.route("/signin")
 def signin():
-   #return "hello hemasai"
+   #return "hello guhan"
    return render_template('signin.html')
 
 if __name__=='__main__':
